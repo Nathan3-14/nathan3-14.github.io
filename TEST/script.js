3846085728bd1fd5c4203550a4f2838d0a1edd1c
Nathan <117750247+Nathan3-14@users.noreply.github.com>
2023-05-10 21:01:22 +0000

    Attempted to tweak ./TEST/script.js (not working on ipad)

--- TEST/script.js
+++ TEST/script.js
@@ -11,19 +11,20 @@
 //     console.log(selectedTheme);
 // });
 
-
+const output = document.getElementById("output");
 const radioButtons = document.querySelectorAll('input[name="theme"]');
-let selectedTheme
+let selectedTheme;
 setInterval(function() {
     for (const radioButton of radioButtons) {
         if (radioButton.checked) {
             selectedTheme = radioButton.value;
             break;
-    }
-    console.log(selectedTheme);};
-},500);
+    };
+    output.innerHTML = selectedTheme;
+}}, 500);
 
-
-// setInterval(function() {
-//     console.log("Hello, world!");
-//   }, 1000);
+/*
+setInterval(function() {
+    console.log("Hello, world!");
+}, 1000);
+*/
