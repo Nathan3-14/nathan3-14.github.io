fae25c54c0033761161ada77730295fc269a0bbd
Nathan <117750247+Nathan3-14@users.noreply.github.com>
2023-05-15 14:47:07 +0000

    Added BCP-Shop

---
+++ bcp-shop/readme.md
@@ -0,0 +1,1 @@
+# Readme of BCP shop
--- script.js
+++ script.js
@@ -1,1 +1,2 @@
 document.getElementById('menu').onclick = () => location.assign('menu');
+document.getElementById('bcp-shop').onclick = () => location.assign('bcp-shop');
