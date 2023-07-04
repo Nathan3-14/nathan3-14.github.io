441154d75fe889aa47c16865df764b1e8b2ba2dc
Nathan <nathan.watson.172@gmail.com>
2023-07-04 17:50:12 +0100

    Implemented theme selection cookies Added basic recipe selector Added basic recipe templated

--- menu/script.js
+++ menu/script.js
@@ -1,23 +1,34 @@
-const themes = ["dark", "light"];
+let getCookie = (cname) => {
+    let output = document.cookie
+    .split('; ')
+    .find((row) => row.startsWith(cname+'='))
+    ?.split('=')[1];
+    return output
+}
+
+
+document.getElementById('style').href='style-'+getCookie('theme')+'.css';
+
+
+const themes = ['dark', 'light'];
 
 for (var i = 0; i < themes.length; i++) {
     let theme = themes[i];
-    document.getElementById(theme).addEventListener("click", () => {
-        document.getElementById("style").href="style-"+theme+".css";
+    document.getElementById(theme).addEventListener('click', () => {
+        document.getElementById('style').href='style-'+theme+'.css';
+        document.cookie = 'theme='+theme+'; SameSite=Strict';
     });
 }
 
 
-const btn = document.querySelector('#btn');        
-const radioButtons = document.querySelectorAll('input[name="size"]');
-btn.addEventListener("click", () => {
-    let selectedSize;
-    for (const radioButton of radioButtons) {
-        if (radioButton.checked) {
-            selectedSize = radioButton.value;
-            break;
-        }
+
+
+const items = ['pasta_sausage', 'brownie'];
+items.forEach(item => {
+    let element = document.getElementById(item);
+    if (element != null) {
+        element.addEventListener('click', (event) => {
+            window.location = '/menu/recipes/'+element.id;
+        })
     }
-    // show the output:
-    output.innerText = selectedSize ? `You selected ${selectedSize}` : `You haven't selected any size`;
 });
