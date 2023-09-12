e1d736f8286b8ba33c2b1e069bd7684440d1b851
Nathan <117750247+Nathan3-14@users.noreply.github.com>
2023-09-12 13:21:18 +0000

    Tested automatic link creator

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+var elements = document.getElementsByClassName("link")
+for (i=0; i<elements.len()-1; i++) {
+    var element = elements[i]
+    element.onclick = () => location.assign(element.id)
+}
+/*
 document.getElementById('menu').onclick = () => location.assign('menu');
 document.getElementById('bcp-shop').onclick = () => location.assign('bcp-shop');
+*/
