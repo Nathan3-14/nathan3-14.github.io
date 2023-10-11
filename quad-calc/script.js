4e666d65597b7d1700bd276a10a28ed207b76ab9
Nathan <nathan.watson.172@gmail.com>
2023-10-11 16:57:48 +0100

    Made quadratic calculator work

--- quad-calc/script.js
+++ quad-calc/script.js
@@ -1,16 +1,22 @@
-var in_1 = Document.getElementById("1");
-var in_2 = Document.getElementById("2");
-var in_3 = Document.getElementById("3");
+var in_1 = document.getElementById("1");
+var in_2 = document.getElementById("2");
+var in_3 = document.getElementById("3");
 
-var dif_1 = in_2 - in_1
-var dif_2 = in_3 - in_2
+
+var dif_1 = in_2.value - in_1.value
+var dif_2 = in_3.value - in_2.value
 var dif_dif_1 = dif_2 - dif_1
 
 
-Document.getElementById("4").addEventListener("click", () => {
-    var a = dif_dif_1 / 2;
-    var b = dif_1 - (3 * a);
-    var c = in_1 - (a + b);
+document.getElementById("4").addEventListener("click", () => {
+    console.log(in_1.value, in_2.value, in_3.value);
 
-    Document.getElementById("output").innerHTML = a.toString() + "n^2 + " + b.toString() + "n + " + c.toString();
+    var a = dif_dif_1 / 2;
+    console.log(dif_1)
+    console.log(3*a);
+    var b = dif_1 - (3 * a);
+    console.log(b);
+    var c = in_1.value - (a + b);
+
+    document.getElementById("output").innerHTML = a.toString() + "n^2 + " + b.toString() + "n + " + c.toString();
 })
