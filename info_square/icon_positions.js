4637c81cc81fc3d40e940c51b7dc2cdffda0f39f
nathan3-14 <nathan.watson.172@gmail.com>
2024-01-10 21:45:18 +0000

    Updated formatting

--- info_square/icon_positions.js
+++ info_square/icon_positions.js
@@ -3,7 +3,7 @@ const set_positions = () => {
     var temperature_icons = document.getElementsByClassName("temperature_icon");
     var left_margin = 30;
     var top_margin = 0;
-    for (let i=0; i<weather_icons.length; i++) {
+    for (let i = 0; i < weather_icons.length; i++) {
         left_margin += 60;
         if (i % 3 == 0) {
             left_margin = 30;
@@ -14,14 +14,14 @@ const set_positions = () => {
         if (i == 9) {
             left_margin += 60;
         }
-        
+
         weather_icons[i].style.marginLeft = left_margin + "px";
         weather_icons[i].style.marginTop = top_margin + "px";
     }
 
     top_margin += 45;
     left_margin = -15;
-    for (let i=0; i<temperature_icons.length; i++){
+    for (let i = 0; i < temperature_icons.length; i++) {
         left_margin += 45;
         temperature_icons[i].style.marginLeft = left_margin + "px";
         temperature_icons[i].style.marginTop = top_margin + "px";
