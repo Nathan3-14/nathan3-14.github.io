6e5a620e52ec20e611c1ff89ff33822bf631702a
Nathan <117750247+Nathan3-14@users.noreply.github.com>
2024-02-07 15:13:14 +0000

    Testing html convert

---
+++ mindmaps/electricity/convert-html.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+
+with open("./index.html", "r") as f:
+    html_code = f.read()
+
+soup = BeautifulSoup(html_code, 'lxml')
+head = soup.find("head")
+body = soup.find("body")
+
+new_title = soup.new_tag("title")
+new_title.string = "Different Title"
+title = soup.find("title")
+
+style_link = soup.new_tag("link")
+style_link["rel"] = "stylesheet"
+style_link["href"] = "../main.css"
+head.append(style_link)
+
+title.replace_with(new_title)
+
+with open("new_index.html", "w") as f:
+    f.write(soup.prettify())
+
+print(soup.prettify())
