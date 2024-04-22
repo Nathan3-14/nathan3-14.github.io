30757ab81ed579415b6c4b7b3a6dfe7c005eac4f
Nathan <117750247+Nathan3-14@users.noreply.github.com>
2024-04-22 16:11:34 +0100

    Updated quad calc (fixed?)

--- quad-calc/script_equation.js
+++ quad-calc/script_equation.js
@@ -1,5 +1,3 @@
-console.log("working")
-
 var in_1 = document.getElementById("1");
 var in_2 = document.getElementById("2");
 var in_3 = document.getElementById("3");
@@ -10,21 +8,23 @@ const calculate_equation = () => {
     dif_1 = in_2.value - in_1.value
     dif_2 = in_3.value - in_2.value
     dif_dif_1 = dif_2 - dif_1
-    
-    var a = dif_dif_1 / 2;
-    var b = dif_1 - (3 * a);
-    var c = in_1.value - (a + b);
-    var abc = [a, b, c]
 
-    document.getElementById("output_equation").innerHTML = a.toString() + "n^2 + " + b.toString() + "n + " + c.toString();
-    var val_ins = document.getElementsByClassName("seq_ins");
-    for (var i = 0; i < val_ins.length; i++) {
-        val_ins[i].value = abc[i];
-    }
+    console.log(in_1.value, in_2.value, in_3.value);
+
+    var a = dif_dif_1 / 2;
+    console.log(dif_1)
+    console.log(3*a);
+    var b = dif_1 - (3 * a);
+    console.log(b);
+    var c = in_1.value - (a + b);
+
+    document.getElementById("output").innerHTML = a.toString() + "n^2 + " + b.toString() + "n + " + c.toString();
+
 }
 
-var ins = document.getElementsByClassName("equ_ins");
+var ins = document.getElementsByClassName("num_ins")
 for (var i = 0; i < ins.length; i++) {
-    ins[i].oninput = calculate_equation;
+    console.log(ins[i])
+    ins[i].addEventListener("change", () => { calculate_equation() })
 }
 
