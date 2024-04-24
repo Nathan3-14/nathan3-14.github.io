8f18f91adea33ba8aa70b2187b0b8bc10dfe28c2
Nathan <117750247+Nathan3-14@users.noreply.github.com>
2024-04-24 17:54:56 +0100

    test

--- servers/velocity/script.js
+++ servers/velocity/script.js
@@ -1,4 +1,8 @@
 var target_location = "http://127.0.0.1:25570";
 var target_location_2 = "https://bbc.co.uk";
 
-window.location.href = target_location;
+
+document.getElementById("test-button").onclick = () => {
+    console.log(target_location);
+    window.location.href = target_location;
+}
