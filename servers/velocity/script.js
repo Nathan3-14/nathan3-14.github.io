94a974abb05e03bff4a9b16770db726b168df454
Nathan <117750247+Nathan3-14@users.noreply.github.com>
2024-04-24 18:01:36 +0100

    Fixed?

--- servers/velocity/script.js
+++ servers/velocity/script.js
@@ -1,7 +1,4 @@
 var target_location = "http://127.0.0.1:25570";
 
 
-console.log(target_location);
-document.getElementById("test-button").onclick = () => {
-    window.location.href = target_location;
-}
+window.location.href = target_location;
