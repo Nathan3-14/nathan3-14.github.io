c6660fc4a6463c4d90f0ad86659fa9cd3882cb73
Nathan3-14 <nathan.watson.172@gmail.com>
2024-08-30 18:30:54 +0100

    works now

--- risk/script.js
+++ risk/script.js
@@ -1,10 +1,37 @@
-// from random import randint
-// from rich.console import Console
+var dice = () => {
+    return Math.floor(Math.random() * 6) + 1; 
+}
+var range = [1, 2, 3, 4]
 
+function results(attack_rolls, defend_rolls, attack_losses, defend_losses) {
+    console.log("resulting")
+    var roll_numbers = ["one", "two", "three", "four", "five", "six"]
 
+    var result_div = document.createElement("div")
+    result_div.classList.add("result_entry")
+    var result_title = document.createElement("h1")
+    result_title.innerHTML = `Battle Results`
+    var comparison_text = document.createElement("h2")
+    comparison_text.innerHTML = "vs"
+    var losses = document.createElement("h3")
+    losses.innerHTML = `Attack lost ${attack_losses}, Defend lost ${defend_losses}`
 
-// def dice() -> int:
-//     return randint(1, 6)
+    var result_attack_rolls = document.createElement("div")
+    for (let i = 0; i<attack_rolls.length; i++) {
+        var roll = document.createElement("i")
+        roll.classList.add("fa-solid", `fa-dice-${roll_numbers[attack_rolls[i]-1]}`, "dice_roll", "attack")
+        result_attack_rolls.append(roll)
+    }
+    var result_defend_rolls = document.createElement("div")
+    for (let i = 0; i<defend_rolls.length; i++) {
+        var roll = document.createElement("i")
+        roll.classList.add("fa-solid", `fa-dice-${roll_numbers[defend_rolls[i]-1]}`, "dice_roll", "defend")
+        result_defend_rolls.append(roll)
+    }
+    result_div.append(result_title, result_attack_rolls, comparison_text, result_defend_rolls ,losses)
+    document.body.append(result_div)
+}
+
 
 var get_winner = (attack_count, defend_count) => {
     var current_attack_count = attack_count - 1
@@ -12,41 +39,59 @@ var get_winner = (attack_count, defend_count) => {
 
 
     while (current_attack_count > 0 && current_defend_count > 0) {
-        attack_dice_count = min(current_attack_count, 3)
-        defend_dice_count = min(current_defend_count, 2)
-    
-        dprint(f"[bright_red]Attacking with {attack_dice_count} [bold](from {attack_count}-1={current_attack_count})[/bold][/bright_red]", lineskip=3)
-        dprint(f"[bright_cyan]Defending with {defend_dice_count} [bold](from {current_defend_count})[/bold][/bright_cyan]")
+        console.log()
+        console.log()
+        attack_dice_count = Math.min(current_attack_count, 3)
+        defend_dice_count = Math.min(current_defend_count, 2)
+
+        console.log(`Attacking with ${attack_dice_count} dice`)
+        console.log(`Defending with ${defend_dice_count} dice`)
         
     
     
-        attack_dice_rolls = [dice() for roll in range(attack_dice_count)]
-        attack_dice_rolls.sort(reverse=True)
-        defend_dice_rolls = [dice() for roll in range(defend_dice_count)]
-        defend_dice_rolls.sort(reverse=True)
+        attack_dice_rolls = Array.from(
+            {length: attack_dice_count},
+            () => dice()
+        );
+        attack_dice_rolls.sort((a, b) => b - a)
+
+        defend_dice_rolls = Array.from(
+            {length: defend_dice_count},
+            () => dice()
+        );
+        defend_dice_rolls.sort((a, b) => b - a)
+
+        console.log(`Attack rolls ${attack_dice_rolls}`)
+        console.log(`Defend rolls ${defend_dice_rolls}`)
     
-        dprint(f"[bright_red]Attack rolled {attack_dice_rolls}[/bright_red]", lineskip=1)
-        dprint(f"[bright_cyan]Defence rolled {defend_dice_rolls}[/bright_cyan]")
-    
-    
-        print("") #? Line break to keep debugging consistency ?#
-        for index in range(min(attack_dice_count, defend_dice_count)):
+        
+        var attack_losses = 0
+        var defend_losses = 0
+        for (let index = 0; index < Math.min(attack_dice_count, defend_dice_count); index++) {
             current_attack_roll = attack_dice_rolls[index]
             current_defend_roll = defend_dice_rolls[index]
-            dprint(f"[bright_green][bright_red]{current_attack_roll}[/bright_red] vs [bright_cyan]{current_defend_roll}[/bright_cyan][/bright_green] ", end="")
-            if current_attack_roll > current_defend_roll:
-                current_defend_count -= 1
-                print(f"[bright_red bold]Attack wins[/bright_red bold]") #! Are not 'dprint' as they continue from previous print statement !#
-            else:
-                current_attack_count -= 1
-                print(f"[bright_cyan bold]Defend wins[/bright_cyan bold]") #! Are not 'dprint' as they continue from previous print statement !#
-        
-    
-    
-        dprint(f"[bright_red]Attack at {current_attack_count}[/bright_red]", lineskip=1)
-        dprint(f"[bright_cyan]Defend at {current_defend_count}[/bright_cyan]")
+            if (current_attack_roll > current_defend_roll) {
+                console.log(`${current_attack_roll} (attack) vs ${current_defend_roll} (defend) ATTACK WINS`)
+                defend_losses += 1
+            } else {
+                console.log(`${current_attack_roll} (attack) vs ${current_defend_roll} (defend) DEFEND WINS`)
+                attack_losses += 1
+            }
+        }
+        current_attack_count -= attack_losses
+        current_defend_count -= defend_losses
+
+        results(attack_dice_rolls, defend_dice_rolls, attack_losses, defend_losses)
     }
     
-    return OutcomeResult(current_attack_count, current_defend_count)
+    document.getElementById("attack_count").innerHTML = current_attack_count.toString()
+    document.getElementById("defend_count").innerHTML = current_defend_count.toString()
 }
 
+
+var get_winner_with_values = () => {
+    get_winner(
+        document.getElementById("start_attack_count").value,
+        document.getElementById("start_defend_count").value
+    )
+}
