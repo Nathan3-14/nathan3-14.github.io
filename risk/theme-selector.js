7f122bef3d0e7156a0b3c83b3ca96616071e3b92
Nathan3-14 <nathan.watson.172@gmail.com>
2024-09-02 18:09:10 +0100

    Made theme auto-select based on start

--- risk/theme-selector.js
+++ risk/theme-selector.js
@@ -1,15 +1,15 @@
 var change_theme = () => {
-    console.log(`Setting theme to ${selected_theme}`)
     if (selected_theme == "light") {
         selected_theme = "dark"
     } else {
         selected_theme = "light"
     }
+    console.log(`Setting theme to ${selected_theme}`)
     document.getElementById("theme").href = `style-${selected_theme}.css`
 }
 
 var theme_change_button = document.createElement("button")
-var selected_theme = "light"
+var selected_theme = document.getElementById("theme").href.split(".")[0].split("-")[1]
 theme_change_button.onclick = change_theme
 theme_change_button.innerHTML = "Change Theme"
 
