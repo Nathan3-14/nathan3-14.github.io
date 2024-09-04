b51a7f18e67f8817f1d2526c13f46d2c6b6b063e
Nathan3-14 <nathan.watson.172@gmail.com>
2024-09-04 18:49:03 +0100

    Fixed links

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var elements = document.getElementsByClassName("link")
-for (i=0; i<elements.len()-1; i++) {
+
+for (i=0; i<elements.length-1; i++) {
     var element = elements[i]
-    element.onclick = () => location.assign(element.id)
+    console.log(element.id)
+    element.addEventListener("click", (event) => {
+        var button = event.target
+        location.assign(button.id)
+    })
 }
-/*
-document.getElementById('menu').onclick = () => location.assign('menu');
-document.getElementById('bcp-shop').onclick = () => location.assign('bcp-shop');
-*/
