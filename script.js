2880d3d27bc4bd2818c90126a37b2c4a960f140f
Nathan3-14 <nathan.watson.172@gmail.com>
2024-09-05 18:53:48 +0100

    Fixed link creation issue

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var elements = document.getElementsByClassName("link")
 
 
-for (i=0; i<elements.length-1; i++) {
+for (i=0; i<elements.length; i++) {
     var element = elements[i]
     console.log(element.id)
     element.addEventListener("click", (event) => {
