7194d650aee2a7fd4c2628145192b03ec42a7a13
Nathan3-14 <nathan.watson.172@gmail.com>
2024-12-05 17:32:17 +0000

    Made new gradients easier to add

---
+++ gradients/gradients.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+if __name__ == "__main__":
+    template = "<h1>Error creating gradient</h1>"
+    with open("template.html", "r") as f:
+        template = f.read()
+
+    colour_filepath = sys.argv[1]
+    
+    data = []
+    with open(colour_filepath, "r") as f:
+        data = f.readlines()
+    
+    for colour_set in data:
+        new_path = "-".join([colour[1:] for colour in colour_set.split(", ")]).strip()
+        print(f"'{new_path}'")
+        if not os.path.exists(new_path):
+            os.mkdir(new_path)
+        with open(f"{new_path}/index.html", "w") as f:
+            f.write(template.replace("'#~acoloursa~#'", colour_set))
+
+        home_page = []
+        with open("index.html", "r") as f:
+            home_page = f.readlines()
+        home_page.insert(8, f"\t<div id={new_path} class=\"gradient-button\"></div>\n")
+        with open("index.html", "w") as f:
+            f.write("".join(home_page))
